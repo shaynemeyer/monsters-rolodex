ac8030a229448faadecdc781184734b58f4d5a10
Shayne Meyer <shayne.meyer@gmail.com>
2019-08-09 08:24:52 -0700

    Updates

---
+++ precache-manifest.89456907a48cc929d6924a79fba2723a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf94801e3b6b670ca5e669d2ce5e47f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c06289e6f0b753ddeffa",
+    "url": "/monsters-rolodex/static/css/main.3d9a6804.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "c06289e6f0b753ddeffa",
+    "url": "/monsters-rolodex/static/js/main.b8a79d85.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
